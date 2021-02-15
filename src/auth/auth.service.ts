69909ef44efc491176bbfbe9eddf3da3e90c4fba
ehdgnl5249 <ehdgnl5249@naver.com>
2021-02-16 00:10:17 +0900

    backend - Auth add mo, co, s

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user/user.repository';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  async signUp() {
+    return this.userRepository.signUp();
+  }
+}
